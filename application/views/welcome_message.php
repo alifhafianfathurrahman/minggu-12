c9652f0806f7bfc6a3c08d63261e3bfb61bdc981
alifhafian <alif.hafian@gmail.com>
2020-06-09 14:22:39 +0700

    menambahkan minggu 12

---
+++ application/views/welcome_message.php
@@ -0,0 +1,6 @@
+<div class="jumbotron jumbotron-fluid">
+	<div class="container">
+		<h1 class="display-4">Tugas Minggu ke 12</h1>
+		<p class="lead">Alif Hafian Fathurrahman A12.2018.06037</p>
+	</div>
+</div>
